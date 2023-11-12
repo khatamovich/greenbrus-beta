73f7ad938062711ae3521145f16d9a2adfdb6eb4
khatamovich <khatamovich@icloud.com>
2023-11-13 00:26:31 +0500

    Emai.js

--- src/components/OrderForm/index.jsx
+++ src/components/OrderForm/index.jsx
@@ -5,27 +5,115 @@ import StyledOrderForm, {
   Submit,
 } from "./OrderForm.styled.js";
 
+import { useState } from "react";
+
+import emailjs from "@emailjs/browser";
+
 const OrderForm = () => {
+  const [sucess, setSuccess] = useState(false);
+  const [order, setOrder] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        "service_t3kynhp",
+        "template_dzebfjg",
+        { ...order },
+        "GVL0H9Lo_0Y6Nb39B",
+      )
+      .then(
+        (result) => {
+          if (result.text.includes("OK")) {
+            setSuccess(true);
+            alert("Ваш заказ успешно оформлен.");
+            setOrder({
+              name: "",
+              email: "",
+              phone: "",
+              message: "",
+            });
+          }
+        },
+        (error) => {
+          setSuccess(false);
+          alert(
+            "Произошла неизвестная ошибка при оформлении заказа. Обратитесь к технической поддержке.",
+          );
+        },
+      )
+      .finally(() => {
+        setTimeout(() => {
+          setSuccess(false);
+        }, 3000);
+      });
+  };
+
   return (
-    <StyledOrderForm>
+    <StyledOrderForm onSubmit={sendEmail}>
       <Label>
         Ваше имя
-        <Field type="text" />
+        <Field
+          value={order.name}
+          onChange={(event) =>
+            setOrder((prevOrder) => ({
+              ...prevOrder,
+              name: event.target.value,
+            }))
+          }
+          type="text"
+        />
       </Label>
 
       <Label>
         Ваш - E-mail
-        <Field type="email" required max="50" />
+        <Field
+          value={order.email}
+          onChange={(event) =>
+            setOrder((prevOrder) => ({
+              ...prevOrder,
+              email: event.target.value,
+            }))
+          }
+          type="email"
+          required
+          max="50"
+        />
       </Label>
 
       <Label>
         Ваш номер телефона
-        <Field type="text" required />
+        <Field
+          value={order.phone}
+          onChange={(event) =>
+            setOrder((prevOrder) => ({
+              ...prevOrder,
+              phone: event.target.value,
+            }))
+          }
+          type="text"
+          required
+        />
       </Label>
 
       <Label>
         Ваше сообщение / подробности
-        <Message placeholder="50 х 70 x 3м  Первый сорт  ГОСТ  Хвоя  95 шт." />
+        <Message
+          value={order.message}
+          onChange={(event) =>
+            setOrder((prevOrder) => ({
+              ...prevOrder,
+              message: event.target.value,
+            }))
+          }
+          placeholder="50 х 70 x 3м  Первый сорт  ГОСТ  Хвоя  95 шт."
+        />
       </Label>
 
       <Submit type="submit">Оформить заказ</Submit>
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,13 +3,13 @@ import Advantages from "../../components/Advantages";
 import Intro from "../../components/Intro";
 
 const HomePage = () => {
-   return (
-      <>
-         <About />
-         <Advantages />
-         <Intro />
-      </>
-   );
+  return (
+    <>
+      <About />
+      <Advantages />
+      <Intro />
+    </>
+  );
 };
 
 export default HomePage;
